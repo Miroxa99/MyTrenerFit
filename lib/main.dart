375889d6007c286ed409c395416381cc4eee709d
Miroxa99 <Miroshkinkir@yandex.ru>
2022-06-13 09:58:09 +0300

    Создал основной класс

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,12 @@
-import 'package:flutter/material.dart'
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyFitnesApp());
+
+class MyFitnesApp extends StatelessWidget {
+  const MyFitnesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
